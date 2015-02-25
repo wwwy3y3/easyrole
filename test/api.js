679cb26b9f5813b932fe3a2f5aecf6f9bd5b9230
william <wwwy3y3@gmail.com>
2015-02-25 23:45:21 +0800

    tests fit new api

--- test/api.js
+++ test/api.js
@@ -27,39 +27,39 @@ rbac.users.allow({
 // tests
 describe('admins', function () {
 	it('should have all permissions', function () {
-		rbac.can('admins').do('create.blogs').should.be.true;
-		rbac.can('admins').do('update.settings').should.be.true;
+		rbac.can('admins').do('blogs.create').should.be.true;
+		rbac.can('admins').do('settings.update').should.be.true;
 	})
 })
 
 
 describe('editors', function () {
 	it('should create a blog', function () {
-		rbac.can('editors').do('create.blogs').should.be.true;
+		rbac.can('editors').do('blogs.create').should.be.true;
 	})
 
 	it('should not be allowed to update settings', function () {
-		rbac.can('editors').do('update.settings').should.be.false;
+		rbac.can('editors').do('settings.update').should.be.false;
 	})
 })
 
 describe('developers', function () {
 	it('should create a blog', function () {
-		rbac.can('developers').do('create.blogs').should.be.true;
+		rbac.can('developers').do('blogs.create').should.be.true;
 	})
 
 	it('should update settings', function () {
-		rbac.can('developers').do('update.settings').should.be.true;
+		rbac.can('developers').do('settings.update').should.be.true;
 	})
 })
 
 describe('users', function () {
 	it('should read a blog', function () {
-		rbac.can('users').do('read.blogs').should.be.true;
+		rbac.can('users').do('blogs.read').should.be.true;
 	})
 
 	it('should not be allowed to create a blog', function () {
-		rbac.can('users').do('create.blogs').should.be.false;
+		rbac.can('users').do('blogs.create').should.be.false;
 	})
 })
 
@@ -71,10 +71,10 @@ describe('get roles', function () {
 	})
 
 	it('should use admin object to test permissions', function () {
-		admin.can('create.blogs').should.be.true;
+		admin.can('blogs.create').should.be.true;
 	})
 
 	it('should use user object to test permissions', function () {
-		rbac.getRoles('users').can('create.blogs').should.be.false;
+		rbac.getRoles('users').can('blogs.create').should.be.false;
 	})
 })
